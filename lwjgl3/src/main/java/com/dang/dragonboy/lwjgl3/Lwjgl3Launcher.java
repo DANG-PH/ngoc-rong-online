69edae38fbaff952d700a0705bcb920dddbab407
DANG-PH <dangph.ptit@gmail.com>
2025-07-15 12:40:29 +0700

    fix laucher

--- lwjgl3/src/main/java/com/dang/dragonboy/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/dang/dragonboy/lwjgl3/Lwjgl3Launcher.java
@@ -4,36 +4,25 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.dang.dragonboy.he_thong.Main;
 
-/** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
     public static void main(String[] args) {
-        if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
-        createApplication();
+        if (StartupHelper.startNewJvmIfRequired()) return;
+        taoUngDung();
     }
 
-    private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new Main(), getDefaultConfiguration());
+    private static Lwjgl3Application taoUngDung() {
+        return new Lwjgl3Application(new Main(), layCauHinh());
     }
 
-    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
-        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
-        configuration.setTitle("Github DANG-PH");
-        //// Vsync limits the frames per second to what your hardware can display, and helps eliminate
-        //// screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
-        configuration.useVsync(true);
-        //// Limits FPS to the refresh rate of the currently active monitor, plus 1 to try to match fractional
-        //// refresh rates. The Vsync setting above should limit the actual FPS to match the monitor.
-        configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
-        //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
-        //// useful for testing performance, but can also be very stressful to some hardware.
-        //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
-
-        configuration.setWindowedMode(1020, 610);
-        //// You can change these files; they are in lwjgl3/src/main/resources/ .
-        //// They can also be loaded from the root of assets/ .
-        configuration.setWindowIcon("hud/giaodienngoai/chung/icon128_1.png", "hud/giaodienngoai/chung/icon32_1.png", "hud/giaodienngoai/chung/icon16_1.png");
-        configuration.setResizable(false);   // Không cho kéo giãn cửa sổ
-        configuration.setMaximized(false);   // Không cho phóng to toàn màn hình
-        return configuration;
+    private static Lwjgl3ApplicationConfiguration layCauHinh() {
+        Lwjgl3ApplicationConfiguration cauHinh = new Lwjgl3ApplicationConfiguration();
+        cauHinh.setTitle("Github DANG-PH");
+        cauHinh.useVsync(true);
+        cauHinh.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
+        cauHinh.setWindowedMode(1020, 610);
+        cauHinh.setWindowIcon("hud/giaodienngoai/chung/icon128_1.png", "hud/giaodienngoai/chung/icon32_1.png", "hud/giaodienngoai/chung/icon16_1.png");
+        cauHinh.setResizable(false);   // Không cho kéo giãn cửa sổ
+        cauHinh.setMaximized(false);   // Không cho phóng to toàn màn hình
+        return cauHinh;
     }
 }
