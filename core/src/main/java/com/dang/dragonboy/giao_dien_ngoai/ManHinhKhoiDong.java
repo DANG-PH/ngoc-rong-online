97e7902a116f4d05652273f3849fa2255504186c
DANG-PH <dangph.ptit@gmail.com>
2025-07-15 08:47:48 +0700

    update logogame

--- core/src/main/java/com/dang/dragonboy/giao_dien_ngoai/ManHinhKhoiDong.java
+++ core/src/main/java/com/dang/dragonboy/giao_dien_ngoai/ManHinhKhoiDong.java
@@ -16,7 +16,7 @@ public class ManHinhKhoiDong implements Screen {
     //bắt buộc phải truyền kiểu class Main vào vì java là nn lập trình tĩnh , bên kia this ko cần vì nằm trong class main rồi
     public ManHinhKhoiDong(Main game) {
         this.game = game; //ko có dòng này thì game của class = null nên setScreen ko có tác dụng phải gán để các file sau còn xài được render và các thứ của hàm Main
-        logogame = new Texture("hud/giaodienngoai/chung/logogame.jpg");
+        logogame = new Texture("hud/giaodienngoai/chung/logogame.png");
         batch = new SpriteBatch();
     }
 
@@ -31,8 +31,8 @@ public class ManHinhKhoiDong implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batch.begin();
-        float scaledWidth = 350;
-        float scaledHeight = 350;
+        float scaledWidth = logogame.getWidth();
+        float scaledHeight = logogame.getHeight();
         float x = (Gdx.graphics.getWidth() - scaledWidth) / 2f;
         float y = (Gdx.graphics.getHeight() - scaledHeight) / 2f;
         batch.draw(logogame, x, y, scaledWidth, scaledHeight);
